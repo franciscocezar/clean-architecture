ee960b4ef7811f8edeb11479b2af7e4fe88a3174
Francisco Cezar <franciscocezar.jr@outlook.com>
2023-03-29 19:46:40 -0300

    test: Testing RegisterPetController

--- src/presenters/controllers/register_pet_controller_test.py
+++ src/presenters/controllers/register_pet_controller_test.py
@@ -40,3 +40,97 @@ def test_handle():
     # Testing outputs
     assert response.status_code == 200
     assert "error" not in response.body
+
+
+def test_route_error_no_body():
+    """Testing handle in RegisnterPetController"""
+
+    register_pet_use_case = RegisterPetSpy(PetRepositorySpy(), None)
+    register_pet_handle = RegisterPetController(register_pet_use_case)
+
+    response = register_pet_handle.handle(HttpRequest())
+
+    print(response)
+
+    # Testing inputs
+    assert register_pet_use_case.registry_params == {}
+
+    # Testing outputs
+    assert response.status_code == 400
+    assert "error" in response.body
+
+
+def test_route_error_wrong_body():
+    """Testing handle in RegisnterPetController"""
+
+    register_pet_use_case = RegisterPetSpy(PetRepositorySpy(), None)
+    register_pet_handle = RegisterPetController(register_pet_use_case)
+
+    attributes = {"name": faker.word()}
+
+    response = register_pet_handle.handle(HttpRequest(body=attributes))
+
+    print(response)
+
+    # Testing inputs
+    assert register_pet_use_case.registry_params == {}
+
+    # Testing outputs
+    assert response.status_code == 422
+    assert "error" in response.body
+
+
+def test_route_error_without_user_information():
+    """Testing handle in RegisnterPetController"""
+
+    register_pet_use_case = RegisterPetSpy(PetRepositorySpy(), None)
+    register_pet_handle = RegisterPetController(register_pet_use_case)
+
+    attributes = {
+        "name": faker.word(),
+        "specie": "dog",
+        "age": faker.random_number(),
+        "user_information": {},
+    }
+
+    response = register_pet_handle.handle(HttpRequest(body=attributes))
+
+    print(response)
+
+    # Testing inputs
+    assert register_pet_use_case.registry_params == {}
+
+    # Testing outputs
+    assert response.status_code == 422
+    assert "error" in response.body
+
+
+def test_route_error_without_user_id():
+    """Testing handle in RegisnterPetController"""
+
+    register_pet_use_case = RegisterPetSpy(PetRepositorySpy(), None)
+    register_pet_handle = RegisterPetController(register_pet_use_case)
+
+    attributes = {
+        "name": faker.word(),
+        "specie": "dog",
+        "age": faker.random_number(),
+        "user_information": {"user_name": faker.word()},
+    }
+
+    response = register_pet_handle.handle(HttpRequest(body=attributes))
+
+    print(response)
+
+    # Testing inputs
+    assert register_pet_use_case.registry_params["name"] == attributes["name"]
+    assert register_pet_use_case.registry_params["specie"] == attributes["specie"]
+    assert register_pet_use_case.registry_params["age"] == attributes["age"]
+    assert (
+        register_pet_use_case.registry_params["user_information"]
+        == attributes["user_information"]
+    )
+
+    # Testing outputs
+    assert response.status_code == 200
+    assert "error" not in response.body
