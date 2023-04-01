12f6312d96212ecc554706540442b15a2e89aa03
Francisco Cezar <franciscocezar.jr@outlook.com>
2023-04-01 14:53:11 -0300

    feat: Implementing api_adpter

--- src/main/routes/api_route.py
+++ src/main/routes/api_route.py
@@ -1,12 +1,30 @@
-from flask import Blueprint, jsonify
-
-# from src.main.composer import register_user_composer
+from flask import Blueprint, jsonify, request
+from src.main.composer import register_user_composer
+from src.main.adapter import flask_adapter
 
 api_routes_bp = Blueprint("api_routes", __name__)
 
 
-@api_routes_bp.route("/api", methods=["GET"])
-def something():
-    """Teste"""
+@api_routes_bp.route("/api/users", methods=["Post"])
+def register_user():
+    """register user route"""
 
-    return jsonify({"Programdor": "Ninho"})
+    message = {}
+    response = flask_adapter(request=request, api_route=register_user_composer())
+
+    if response.status_code < 300:
+        message = {
+            "Type": "users",
+            "id": response.body.id,
+            "attributes": {"name": response.body.name},
+        }
+
+        return jsonify({"data": message}), response.status_code
+
+    # Handling Errors
+    return (
+        jsonify(
+            {"error": {"status": response.status_code, "title": response.body["error"]}}
+        ),
+        response.status_code,
+    )
